bbbc407756e81600edae70484859ce59f1afced4
Denis Rystsov <rystsov.denis@gmail.com>
2017-02-24 01:11:57 -0800

    adds etcd version

--- cockroachdb/src/RetriableClient.js
+++ cockroachdb/src/RetriableClient.js
@@ -1,45 +1,6 @@
 const pg = require('pg');
 
-class RethinkKV {
-    constructor(pool) {
-        this.pool = pool;
-    }
-    async read(key) {
-        try {
-            const client = await this.pool.connect();
-            const sql = "SELECT * FROM storage WHERE key = $1::text";
-            const result = await query(client, sql, [key]);
-            return result.length == 0 ? null : result[0].value
-        } catch (e) {
-            this.pool.retry();
-            throw e;
-        }
-    }
-    async create(key, value) {
-        try {
-            const client = await this.pool.connect();
-            const sql = "INSERT INTO storage VALUES ($1::text, $2::text)";
-            await query(client, sql, [key, value]);
-            return true;
-        } catch (e) {
-            this.pool.retry();
-            throw e;
-        }
-    }
-    async update(key, value) {
-        try {
-            const client = await this.pool.connect();
-            const sql = "UPDATE storage SET value = $2::text WHERE key = $1::text";
-            await query(client, sql, [key, value]);
-            return true;
-        } catch (e) {
-            this.pool.retry();
-            throw e;
-        }
-    }
-}
-
-class ResettablePool {
+class RetriableClient {
     constructor(config) {
         this.config = config;
         this.client = null;
