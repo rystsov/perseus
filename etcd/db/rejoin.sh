ae8dde32c17a11ea091060a43ad6b89c74838981
Denis Rystsov <rystsov.denis@gmail.com>
2018-01-24 08:56:48 -0800

    Improves diagnostic messages

--- etcd/db/rejoin.sh
+++ etcd/db/rejoin.sh
@@ -12,9 +12,9 @@ if [ -z "$etcd1" ] ; then exit 1 ; fi;
 if [ -z "$etcd2" ] ; then exit 1 ; fi;
 if [ -z "$etcd3" ] ; then exit 1 ; fi;
 
-python -c "for ip in (set(['$etcd1', '$etcd2', '$etcd3']) - set(['$myip'])): print ip" | xargs -I '{}' echo "Isolating: {}"
+python -c "for ip in (set(['$etcd1', '$etcd2', '$etcd3']) - set(['$myip'])): print ip" | xargs -I '{}' echo "Rejoining: {}"
 
 python -c "for ip in (set(['$etcd1', '$etcd2', '$etcd3']) - set(['$myip'])): print ip" | xargs -I '{}' iptables -D INPUT -s '{}' -j DROP
 python -c "for ip in (set(['$etcd1', '$etcd2', '$etcd3']) - set(['$myip'])): print ip" | xargs -I '{}' iptables -D OUTPUT -d '{}' -j DROP
 
-echo "Isolated"
+echo "Rejoined"
