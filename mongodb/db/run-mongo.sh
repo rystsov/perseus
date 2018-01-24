2b29c9cd4c525571c0eddce7925d5e41c0c461ad
Denis Rystsov <rystsov.denis@gmail.com>
2018-01-24 08:57:48 -0800

    Improves diagnostic

--- mongodb/db/run-mongo.sh
+++ mongodb/db/run-mongo.sh
@@ -12,4 +12,9 @@ if [ -z "$mongo1" ] ; then exit 1 ; fi;
 if [ -z "$mongo2" ] ; then exit 1 ; fi;
 if [ -z "$mongo3" ] ; then exit 1 ; fi;
 
-/mongo/mongodb-linux-x86_64-3.6.1/bin/mongod --dbpath /mongo/mem --replSet rs0 --port 27017 --bind_ip 0.0.0.0 > /mongo/logs/$me.log
+echo "me: $me" >> /mongo/logs/$me.log
+echo "mongo1: $mongo1" >> /mongo/logs/$me.log
+echo "mongo2: $mongo2" >> /mongo/logs/$me.log
+echo "mongo3: $mongo3" >> /mongo/logs/$me.log
+
+/mongo/mongodb-linux-x86_64-3.6.1/bin/mongod --dbpath /mongo/mem --replSet rs0 --port 27017 --bind_ip 0.0.0.0 >> /mongo/logs/$me.log
